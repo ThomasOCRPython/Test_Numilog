90ed73c1942034de06af47389ab0af90aa13b4f3
Thomas2020 <thomas.duval2020@campus-eni.fr>
2022-04-01 14:58:48 +0200

    Ajout configuration chemin fichier et destination

--- find_and_copy_files.py
+++ find_and_copy_files.py
@@ -1,40 +1,70 @@
 import os
-import shutil
+import shutil 
+import docx
+
 from list_files import list_pdf
 
-# path='C:/Users/Latitude/Desktop/test'
+# path_disk = "C:/Users/Latitude/Desktop/test"
+# listing = "C:/Users/Latitude/Desktop/test/test"
+destination = "C:/Users/Latitude/Desktop/test/destination"
 
 
-def find_and_copy_files(path_disk):
-    create_folder()
+def find_and_copy_files(path_disk, listing=list_pdf, path_destination=destination):
+    create_folder(path_destination)
+    listing_pdf = path_list_file(listing)
     for root, directories, files in os.walk(path_disk, topdown=False):
         list_file = [os.path.join(root, name) for name in files]
-        for i in list_pdf:
+        for i in listing_pdf:
             for j in list_file:
                 if i == j[-17:]:
                     file = j[-17:]
                     fullpath = j
-                    if most_recent_file(file, fullpath):
+                    if most_recent_file(file, fullpath, path_destination):
                         pass
                     else:
-                        shutil.copy(fullpath, "Mes100Fichiers")
+                        path = path_destination + "/" + "Mes100Fichiers"
+                        shutil.copy(fullpath, path)
 
 
-def most_recent_file(file, fullpath):
-    directory = "Mes100Fichiers"
+def most_recent_file(file, fullpath, path_destination):
+    directory = path_destination + "/" + "Mes100Fichiers"
     for filename in os.listdir(directory):
         f = os.path.join(directory, filename)
-        if f[15:] == file:
+        if f[-17:] == file:
+
             file_directory = os.stat(f)
             file_extern = os.stat(fullpath)
-            if file_extern.st_atime < file_directory.st_atime:
+            if file_extern.st_atime <= file_directory.st_atime:
                 return True
 
 
-def create_folder():
-    if not os.path.exists("Mes100Fichiers"):
-        os.mkdir("Mes100Fichiers")
+def create_folder(path_destination):
+    directory = "Mes100Fichiers"
+
+    parent_dir = path_destination
+
+    path = os.path.join(parent_dir, directory)
+
+    if not os.path.exists(path):
+        os.mkdir(path)
 
 
-# find_and_copy_files(path)
+def path_list_file(listing):
+    directory = listing
+    list_pdf = []
+    for root, dirs, files in os.walk(directory):
+        listpdf = [os.path.join(root, filename) for filename in files]
+        for path_docx in listpdf:
+            if path_docx[-5:] == ".docx":
+                pathdocx = path_docx
+                doc = docx.Document(pathdocx)
+                result = [p.text for p in doc.paragraphs]
+                for file in result:
+                    if file[-4:] == ".PDF" and len(file) == 17:
+                        list_pdf.append(file)
+    return list_pdf
+
+
+# path_list_file(listing)
+# find_and_copy_files(path_disk, listing, path_destination)
 # most_recent_file()
--- view.py
+++ view.py
@@ -3,4 +3,38 @@ from find_and_copy_files import find_and_copy_files
 
 def ask_disk():
     disk = input("Entrez le chemin de votre disk : ")
-    find_and_copy_files(disk)
+    list = input(
+        "Tapez '1' pour entrer le chemin de la liste à trouver, tapez '2' pour utiliser la liste par défaut"
+    )
+    while list not in ("1", "2"):
+        list = input(
+            "Tapez '1' pour entrer le chemin de la liste à trouver, tapez '2' pour utiliser la liste par défaut"
+        )
+    list_file = list_file_find(list)
+
+    folder = input(
+        "Tapez 1 pour choisir le chemin de destination, tapez 2 pour un chemin de destination par defaut"
+    )
+    while folder not in ("1", "2"):
+        folder = input(
+            "Tapez 1 pour choisir le chemin de destination, tapez 2 pour un chemin de destination par defaut"
+        )
+    destination = folder_destination(folder)
+
+    find_and_copy_files(disk, list_file, destination)
+
+
+def list_file_find(nb):
+    if nb == "1":
+        list_file = input("Entrez le chemin de la list à trouver : ")
+        return list_file
+    else:
+        pass
+
+
+def folder_destination(nbr):
+    if nbr == "1":
+        destination = input("Entrez le chemin de destination : ")
+        return destination
+    else:
+        pass
